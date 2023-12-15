353ab0dc75f60ead84931210d0bf895a13d5910f
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-12-15 23:58:54 +0100

    фррффррффр

--- src/FEEDBACK FORM/feedback.jsx
+++ src/FEEDBACK FORM/feedback.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import css from './feedback.module.css'
 
-export const FeedBack = () => {
+class FeedBack extends React.Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
 
+}
+render() {
+    return (
+       <>
+       <h2>
+            Please leave feedaback
+        </h2>
+        <button type='button'></button>
+        <button type='button'></button>
+        <button type='button'></button>
+        </>
+
+    )
 }
